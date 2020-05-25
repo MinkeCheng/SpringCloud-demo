e9556f7e2b8b9e9450bb09a0d8b227a79622cae1
MinkeCheng <chengzhimin@qq.com>
2020-05-25 17:06:32 +0800

    test

--- account-service/src/main/java/com/czm/accountservice/action/OrderHandelAction.java
+++ account-service/src/main/java/com/czm/accountservice/action/OrderHandelAction.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ */
 @RestController
 @RequestMapping(value = "order")
 public class OrderHandelAction {
